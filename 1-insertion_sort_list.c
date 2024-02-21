11481386040663254605afc2d12ef0428dec38e6
flamonrose23 <fatinemar20@icloud.com>
2024-02-21 05:15:04 -0800

    Refreshing some tasks

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,37 +1,38 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - a function that sorts a doubly linked list of integers
- * in ascending order using the Insertion sort algorithm
+ * insertion_sort_list - sorting doubly linked list of integers
+ * using Insertion sort algorithm
  * @list: The list to be sorted
  */
+
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *prev;
+	listint_t *curr, *prev;
 
 	if (!(*list) || !((*list)->next))
 		return;
 
-	current = (*list)->next;
-	while (current)
+	curr = (*list)->next;
+	while (curr)
 	{
-		prev = current->prev;
-		while (prev && prev->n > current->n)
+		prev = curr->prev;
+		while (prev && prev->x > curr->x)
 		{
 			if (prev->prev)
-				prev->prev->next = current;
-			if (current->next)
-				current->next->prev = prev;
-			prev->next = current->next;
-			current->prev = prev->prev;
-			prev->prev = current;
-			current->next = prev;
+				prev->prev->next = curr;
+			if (curr->next)
+				curr->next->prev = prev;
+			prev->next = curr->next;
+			curr->prev = prev->prev;
+			prev->prev = curr;
+			curr->next = prev;
 
-			prev = current->prev;
+			prev = curr->prev;
 			if (!prev)
-				*list = current;
+				*list = curr;
 			print_list(*list);
 		}
-		current = current->next;
+		curr = curr->next;
 	}
 }
